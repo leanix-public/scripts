b965c0188e67d774679f96f454d0dd2183593ae8
Julian Ludwig <julian.ludwig@leanix.net>
2018-07-27 09:11:07 +0200

    Inital upload deleteTag

---
+++ deleteTags/deleteTag.py
@@ -0,0 +1,43 @@
+import json 
+import requests 
+import pandas as pd
+
+api_token = '<APITOKEN>'
+auth_url = 'https://app.leanix.net/services/mtm/v1/oauth2/token' cd
+request_url = 'https://app.leanix.net/services/pathfinder/v1/graphql' 
+
+# Get the bearer token - see https://dev.leanix.net/v4.0/docs/authentication
+response = requests.post(auth_url, auth=('apitoken', api_token),
+                         data={'grant_type': 'client_credentials'})
+response.raise_for_status() 
+access_token = response.json()['access_token']
+auth_header = 'Bearer ' + access_token
+header = {'Authorization': auth_header}
+
+# General function to call GraphQL given a query
+def call(query):
+  data = {"query" : query}
+  json_data = json.dumps(data)
+  response = requests.post(url=request_url, headers=header, data=json_data)
+  response.raise_for_status()
+  return response.json()
+
+# Delete the document
+def deleteTag(id):
+  query = """
+mutation {
+  deleteTag(id: "%s") {
+    id
+  }
+}
+  """ % (id)
+  print ("delete " + id)
+  response = call(query)
+  print (response)
+
+# Start of the main program
+
+df = pd.read_csv('Book1.csv',sep=';')
+  
+for index, row in df.iterrows():
+  deleteTag(row['id'])
