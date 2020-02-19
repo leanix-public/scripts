7abe33e2c3f530d9744616f3ee7f57637b3e2bb6
Christian Zapata <christianzapata@LXBS0037s-MacBook-Pro.local>
2020-02-19 15:44:23 -0500

    [CSES-815] Changed lib usage to lxpy

--- tag2attributes/project/tag2Attributes.py
+++ tag2attributes/project/tag2Attributes.py
@@ -2,38 +2,36 @@ import json
 import requests 
 import pandas as pd
 import math
+import json
+from os import environ
+import lxpy
 
 
 mtm_base_url = 'https://svc.leanix.net/services/mtm/v1' 
 pathfinder_base_url = 'https://adidas.leanix.net/services/pathfinder/v1'
 
-#Authorization
-def getApiToken():
-  with open('../access.json') as json_file:  
-    data = json.load(json_file)
-    return data['apitoken']
+# Authorization
+config = lxpy.ClientConfiguration(
+  base_url=environ.get('BASE_URL', 'us.leanix.net'),
+  api_token=environ.get('API_TOKEN', 'YSCOXQbwROr8XMWGutW3NAhtpEsuKsWtH3JDPSX3')
+)
 
-def getAccessToken(api_token):
-  #different than callPost since it needs to send the auth_header
-  response = requests.post(mtm_base_url+"/oauth2/token", auth=('apitoken', api_token),
-                         data={'grant_type': 'client_credentials'})
-  response.raise_for_status() 
-  access_token = response.json()['access_token']
-  return access_token
-
-def getHeader(access_token):
-  return {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'}
+# Setup for Pathfinder API
+pathfinder = lxpy.Pathfinder(config)
 
 # General function to call GraphQL given a query
-def call(query, access_token):
+def call(query):
   data = {"query" : query}
-  json_data = json.dumps(data)
-  response = requests.post(url=pathfinder_base_url+ '/graphql', headers=getHeader(access_token), data=json_data)
-  response.raise_for_status()
-  return response.json()
+  response = pathfinder.graphql().process_graph_ql(data)
+  # print(response)
+  # import pdb; pdb.set_trace()
+  return(response.to_dict())
+
 
 # Read all existing Application - IT Component relations
-def getAllApps(access_token):
+# Comment from CHZ: while variables isn't needed in the query here,
+# leaving it in the query so others know it's an option
+def getAllApps():
   query = """
   {
     allFactSheets(factSheetType: BusinessCapability) {
@@ -46,49 +44,49 @@ def getAllApps(access_token):
     }
   }
   """
-  response = call(query, access_token)
-  return response['data']['allFactSheets']['edges']
+  response = call(query)
+  return(response)
  
+
 # Start of the main program
-access_token = getAccessToken(getApiToken())
+# access_token = getAccessToken(getApiToken())
 df = pd.read_csv('mapping.csv', sep=';')
 
-apps = getAllApps(access_token)
-for appNode in apps:
+apps = getAllApps()
+for appNode in apps['data']['allFactSheets']['edges']:
+  tags = list(map(lambda x: x['id'], appNode['node']['tags']))
+  
+  patches = []
+  multiSelects = {}
 
-    tags = list(map(lambda x: x['id'], appNode['node']['tags']))
-    
-    patches = []
-    multiSelects = {}
+  for index, row in df.iterrows():
+    if (row['Tag ID'] in tags):
+      tags.remove(row['Tag ID'])
 
-    for index, row in df.iterrows():
-      if (row['Tag ID'] in tags):
-        tags.remove(row['Tag ID'])
+      if (row['Type'] == 'SINGLE_SELECT'):
+        patches.append("{op: add, path: \"/" + row['Attribute'] + "\", value: \""+ row['Value'] + "\"}")
+      elif (row['Type'] == 'MULTIPLE_SELECT'):
+        if (row['Attribute'] not in multiSelects):
+          multiSelects[row['Attribute']] = []
+        multiSelects[row['Attribute']].append(row['Value'])
 
-        if (row['Type'] == 'SINGLE_SELECT'):
-          patches.append("{op: add, path: \"/" + row['Attribute'] + "\", value: \""+ row['Value'] + "\"}")
-        elif (row['Type'] == 'MULTIPLE_SELECT'):
-          if (row['Attribute'] not in multiSelects):
-            multiSelects[row['Attribute']] = []
-          multiSelects[row['Attribute']].append(row['Value'])
+  for k, v in multiSelects.items():
+    multiSelectString = ",".join(map( lambda x: '\\\"' + x + '\\\"', v))
+    patches.append("{op: replace, path: \"/"+ k +"\", value: \"["+ multiSelectString +"]\"}")
 
-    for k, v in multiSelects.items():
-      multiSelectString = ",".join(map( lambda x: '\\\"' + x + '\\\"', v))
-      patches.append("{op: replace, path: \"/"+ k +"\", value: \"["+ multiSelectString +"]\"}")
- 
-    tagString = ",".join(map(lambda x: '{\\\"tagId\\\": \\\"' + x + '\\\"}', tags))
-    patches.append("{op: replace, path: \"/tags\", value: \"["+ tagString + "]\"}")
+  tagString = ",".join(map(lambda x: '{\\\"tagId\\\": \\\"' + x + '\\\"}', tags))
+  patches.append("{op: replace, path: \"/tags\", value: \"["+ tagString + "]\"}")
 
-    query = """
-        mutation {
-          updateFactSheet(id: "%s", patches: [%s]) {
-            factSheet {
-              id
-            } 
-          }
+  query = """
+      mutation {
+        updateFactSheet(id: "%s", patches: [%s]) {
+          factSheet {
+            id
+          } 
         }
-      """ % (appNode['node']['id'], ",".join(patches))
-    print (query)
-    response = call(query,access_token)
-    print (response)
+      }
+    """ % (appNode['node']['id'], ",".join(patches))
+  print (query)
+  response = call(query)
+  print (response)
 
